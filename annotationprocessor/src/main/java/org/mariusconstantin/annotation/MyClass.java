a3cdad2c937287b456ac921ea7e5a2d717606f04
Marius Constantin <mconstantin@deezer.com>
2015-12-10 21:28:00 +0100

    Project Setup

---
+++ annotationprocessor/src/main/java/org/mariusconstantin/annotation/MyClass.java
@@ -0,0 +1,4 @@
+package org.mariusconstantin.annotation;
+
+public class MyClass {
+}
---
+++ magicoapi/src/main/java/org/mariusconstantin/magicoapi/AbstractMagicoHelper.java
@@ -0,0 +1,8 @@
+package org.mariusconstantin.magicoapi;
+
+/**
+ * Created by Marius on 12/8/2015.
+ */
+public abstract class AbstractMagicoHelper<T> {
+    private T mTarget;
+}
